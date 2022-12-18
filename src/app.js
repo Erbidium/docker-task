570dc0103d3d473c57730f26241a0d05c21fa47e
Oleksandr <OlexandrTanasienko@gmail.com>
2022-12-18 19:20:15 +0200

    Remove hostname

--- src/app.js
+++ src/app.js
@@ -15,6 +15,6 @@ app.get('/health-check', (req, res) => {
     res.send('Health check passed');
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
