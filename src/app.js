83436aea4efc90b9390c3abf1e13877ef3620339
Oleksandr <OlexandrTanasienko@gmail.com>
2022-12-18 18:02:50 +0200

    Add health check and liveness probe

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(`${path}index.html`);
 });
 
+app.get('/health-check',(req,res)=> {
+    res.send('Health check passed');
+ });
+
 app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening on port ${port}`);
 });
